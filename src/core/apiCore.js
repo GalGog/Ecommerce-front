91c7cb14e7adfc8e8454236243a488257ea685b8
GalGog <galynna00@gmail.com>
2020-11-05 11:01:50 +0000

    section 16. Payment Gateway

--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -1,7 +1,5 @@
 import { API } from "../config";
 import queryString from "query-string";
-//import queryString from "query-string";
-
 
 export const getProducts = sortBy => {
     return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
@@ -77,49 +75,49 @@ export const listRelated = productId => {
         .catch(err => console.log(err));
 };
 
-//export const getBraintreeClientToken = (userId, token) => {
-//    return fetch(`${API}/braintree/getToken/${userId}`, {
-//        method: "GET",
-//        headers: {
-//            Accept: "application/json",
-//            "Content-Type": "application/json",
- //           Authorization: `Bearer ${token}`
-//        }
-//    })
-//        .then(response => {
-//            return response.json();
-//        })
-//        .catch(err => console.log(err));
-//};
+export const getBraintreeClientToken = (userId, token) => {
+    return fetch(`${API}/braintree/getToken/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
 
-//export const processPayment = (userId, token, paymentData) => {
-//    return fetch(`${API}/braintree/payment/${userId}`, {
- //       method: "POST",
- //       headers: {
-//            Accept: "application/json",
-//            "Content-Type": "application/json",
- //           Authorization: `Bearer ${token}`
-//        },
-//        body: JSON.stringify(paymentData)
-//    })
-//        .then(response => {
- //           return response.json();
-  //      })
-//        .catch(err => console.log(err));
-//};
+export const processPayment = (userId, token, paymentData) => {
+    return fetch(`${API}/braintree/payment/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(paymentData)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
 
-//export const createOrder = (userId, token, createOrderData) => {
-//    return fetch(`${API}/order/create/${userId}`, {
- //       method: "POST",
- //       headers: {
-//            Accept: "application/json",
- //           "Content-Type": "application/json",
- //           Authorization: `Bearer ${token}`
-//        },
-//        body: JSON.stringify({ order: createOrderData })
-//    })
- //       .then(response => {
- //           return response.json();
- //       })
- //       .catch(err => console.log(err));
-//};
+export const createOrder = (userId, token, createOrderData) => {
+    return fetch(`${API}/order/create/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ order: createOrderData })
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
